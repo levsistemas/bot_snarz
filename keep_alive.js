f24677303193f0271fc6db464f84b0d3f91bd75b
Leandro <levsistemasit@gmail.com>
2024-02-29 21:33:46 -0300

    Nuevo cambio

--- keep_alive.js
+++ keep_alive.js
@@ -1,7 +1,7 @@
 var http = require('http');
-let {client} = require('./bot_snarz.js');
+let {Client} = require('./bot_snarz.js');
 
 http.createServer(function (req, res) {
-  res.write("I'm alive / Estoy vivo " + client.user.username);
+  res.write("I'm alive / Estoy vivo " + Client.user.username);
   res.end();
 }).listen(8080);
