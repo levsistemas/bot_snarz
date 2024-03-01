a787862a80caa9c8f2959e240e46ced1d8f5135a
Leandro <levsistemasit@gmail.com>
2024-02-29 21:59:46 -0300

    New Update 1

--- keep_alive.js
+++ keep_alive.js
@@ -1,7 +1,10 @@
 var http = require('http');
 const { Client } = require('./bot_snarz.js');
 
-http.createServer(function (req, res) {
-  res.write("I'm alive / Estoy vivo " + (Client.user ? Client.user.username: 'Bot no encontrado'));
-  res.end();
-}).listen(8080);
+Client.once('ready', () => {
+  console.log('Bot is readyyyyyyyyyyyyyyyyyyyyyyyyyyyy!'); // Solo para verificar en la consola que el bot está listo
+  http.createServer(function (req, res) {
+    res.write("I'm alive / Estoy vivo " + (Client.user ? Client.user.username: "Bot no encontrado"));
+    res.end();
+  }).listen(8080);
+});
